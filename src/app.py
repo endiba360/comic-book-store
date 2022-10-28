775b51d299c9b74f1e3e08f48bbef39780dafb9e
Enrique <endiba360@gmail.com>
2022-10-27 19:03:40 -0500

    Create method to retrive only desired information about fetched character by given name

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify
-from marvel import characters
 import requests
-
+from flask import Flask, jsonify
+from marvel import characters, marvel_hash, marvel_api_public_key, marvel_timestamp
 
 app = Flask(__name__)
 
@@ -33,13 +32,32 @@ def fetch_all_characters():
     else:
         print('Something went wrong...')
 
-@app.route('/searchComics/<string:comic_name>', methods=['GET'])
+@app.route('/searchComics/<int:comic_id>', methods=['GET'])
 def fetch_one_comic(comic_name):
     pass
 
 @app.route('/searchComics/<string:character_name>', methods=['GET'])
 def fetch_one_character(character_name):
-    pass
+    name = character_name
+    query_one_by_name = f'https://gateway.marvel.com:443/v1/public/characters?ts={marvel_timestamp}&name={name}&apikey={marvel_api_public_key}&hash={marvel_hash}'
+    response = requests.get(query_one_by_name)
+    response_json = response.json()
+    
+    if response_json['code'] == 200:
+        match_character = response_json['data']['results']
+        processed_character = {}
+        
+        for key in match_character:
+            processed_character['id'] = key['id']
+            processed_character['name'] = key['name']
+            processed_character['image'] = key['thumbnail']['path']
+            processed_character['appearances'] = key['comics']['available']
+        print(processed_character)
+        
+        return processed_character
+    else:
+        print('Something went wrong...')
+    
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4000, debug=True)
